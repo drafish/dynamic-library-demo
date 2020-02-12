78f6434d55ce50b82069b888f5ca7a3b50ca8da6
drafish <xwlyy1991@163.com>
2020-02-12 13:17:33 +0800

    添加c++示例代码

---
+++ cpp/hello-jni.cpp
@@ -0,0 +1,17 @@
+#include <jni.h>
+#include "Test.h"
+
+extern "C" {
+    JNIEXPORT jstring JNICALL
+    Java_com_example_jni_TestModule_stringFromJNI( JNIEnv* env, jobject thiz, jstring jstr )
+    {
+        return jstr;
+    }
+
+    JNIEXPORT jint JNICALL
+    Java_com_example_jni_TestModule_intFromJNI( JNIEnv* env, jobject thiz, jint a, jint b )
+    {
+        example::Test test;
+        return test.add(a, b);
+    }
+}
