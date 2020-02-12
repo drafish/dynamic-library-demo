fc4a4379f9f68231c0c50c02c902aca783c46b78
drafish <xwlyy1991@163.com>
2020-02-12 14:14:43 +0800

    初始化ios动态库

---
+++ ios/DynamicLibraryDemoTests/DynamicLibraryDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  DynamicLibraryDemoTests.m
+//  DynamicLibraryDemoTests
+//
+//  Created by 丁吉钦 on 2020/2/12.
+//  Copyright © 2020 drafish. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DynamicLibraryDemoTests : XCTestCase
+
+@end
+
+@implementation DynamicLibraryDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
